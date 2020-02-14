dc1380dc09e990f55cb883547af3dd53f6fd1de3
Valloon427428 <61017400+valloon91234@users.noreply.github.com>
2020-02-14 22:58:54 +0900

    Initial commit

---
+++ RunJavascript/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ClearScript;
+using Microsoft.ClearScript.V8;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunJavascript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            V8ScriptEngine v8 = new V8ScriptEngine();
+            v8.Evaluate(File.ReadAllText("CryptoJS.js"));
+            v8.Evaluate(File.ReadAllText("AEScall.js"));
+            String result = v8.Script.encryptedText;
+            Console.WriteLine(result);
+            Console.ReadKey(false);
+        }
+    }
+}
